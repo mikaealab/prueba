df3f35db4a7f10311f1c07df472c56d575dc60be
NASHELY MIKAELA BETANCOURT TOSCANO <nashely.betancourt6156@utc.edu.ec>
2025-06-30 08:50:54 -0500

    sentings

--- asincronoNB/settings.py
+++ asincronoNB/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(_hz(c6=b_whcw-o*!n_$4=&*yfcm9uwn7u4veixmh)62p-jx*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mika2002.pythonanywhere.com']
 
 
 # Application definition
@@ -116,6 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
